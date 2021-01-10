ad698c0ecdbd644e3d3482ad6810043323a831de
Thomas Le Vaou <thomas.levaou@protonmail.com>
2021-01-10 19:26:33 +0100

    Generation du mot + affichage des lettres

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
+import React, { Component } from 'react'
 import './App.css';
 
+const WORDS = ['TELEPHONE', 'CASQUE', 'ORDINATEUR', 'SOURIS', 'CHAT', 'CHIEN', 'GALETTE', 'SPAGHETTI']
 
 /** @author TLV 27/12/2020
 * @brief Réalisation du jeu du pendu, proposé en TP par OpenClassrooms dans le cadre
 *       de la formation sur React.js
+* @details Voir le Readme pour la spec détaillée
 */
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /** @brief state de notre jeu de pendu
+  * @param word phrase est en state, car il peut changer si l'utilisateur décide
+  *        de relancer une partie.
+  * @param usedLetters est un Set (càd un tableau où chaque valeur doit être unique)
+  *        contenant la liste des lettres entrées par l'utilisateur. */
+  state = {
+    word: this.pickRandomWord(),
+    usedLetters: new Set()
+  }
+
+  // Sélection d'un mot aléatoire dans la liste
+  pickRandomWord () {
+    return WORDS[Math.floor(Math.random() * WORDS.length)]
+  }
+
+  /* Fonction fournie dans l'énoncé pour afficher les underscores ou les lettres trouvées
+  *  Rappel: g permet de rechercher plusieurs fois dans une chaîne */
+  computeDisplay (phrase, usedLetters) {
+    return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? ' ' + letter + ' ' : ' _ '))
+  }
+
+  render () {
+    const {word, usedLetters} = this.state
+    return (
+      <div>
+        { this.computeDisplay(word, usedLetters) }
+      </div>
+    )
+  }
 }
 
 export default App;
